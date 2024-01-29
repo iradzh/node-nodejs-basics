927e29207fe8c11a2875740f2a862dc956de0f3b
Iryna Zhebryk <irinazhebrik@gmail.com>
2024-01-29 16:58:02 +0100

    feat: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+
+  for (let i = 0; i < args.length; i += 2) {
+    const name = args[i].replace(/^--/, '');
+    const value = args[i + 1];
+    console.log(`${name} is ${value}`);
+  }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVars = process.env;
+
+  for (const [key, value] of Object.entries(envVars)) {
+    if (key.slice(0, 3) === 'RSS') {
+      console.log(`${key} = ${value}`);
+    }
+  }
 };
 
 parseEnv();
